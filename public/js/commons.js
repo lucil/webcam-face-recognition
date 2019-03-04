2d973625dcc173056fb000b7250226e818100e5e
Emil Trebicka <emil.trebicka@gmail.com>
2019-03-04 11:02:08 +0100

    removed jquery and materialize dependency

--- public/js/commons.js
+++ public/js/commons.js
@@ -21,26 +21,3 @@ async function requestExternalImage(imageUrl) {
     throw new Error("failed to load image from url: " + imageUrl);
   }
 }
-
-function renderSelectList(
-  selectListId,
-  onChange,
-  initialValue,
-  renderChildren
-) {
-  const select = document.createElement("select");
-  $(selectListId)
-    .get(0)
-    .appendChild(select);
-  renderChildren(select);
-  $(select).val(initialValue);
-  $(select).on("change", e => onChange(e.target.value));
-  $(select).material_select();
-}
-
-function renderOption(parent, text, value) {
-  const option = document.createElement("option");
-  option.innerHTML = text;
-  option.value = value;
-  parent.appendChild(option);
-}
--- public/js/faceDetectionControls.js
+++ public/js/faceDetectionControls.js
@@ -22,53 +22,6 @@ function getFaceDetectorOptions() {
     : new faceapi.MtcnnOptions({ minFaceSize });
 }
 
-function onIncreaseMinConfidence() {
-  minConfidence = Math.min(faceapi.round(minConfidence + 0.1), 1.0);
-  $("#minConfidence").val(minConfidence);
-  updateResults();
-}
-
-function onDecreaseMinConfidence() {
-  minConfidence = Math.max(faceapi.round(minConfidence - 0.1), 0.1);
-  $("#minConfidence").val(minConfidence);
-  updateResults();
-}
-
-function onInputSizeChanged(e) {
-  changeInputSize(e.target.value);
-  updateResults();
-}
-
-function changeInputSize(size) {
-  inputSize = parseInt(size);
-
-  const inputSizeSelect = $("#inputSize");
-  inputSizeSelect.val(inputSize);
-  inputSizeSelect.material_select();
-}
-
-function onIncreaseScoreThreshold() {
-  scoreThreshold = Math.min(faceapi.round(scoreThreshold + 0.1), 1.0);
-  $("#scoreThreshold").val(scoreThreshold);
-  updateResults();
-}
-
-function onDecreaseScoreThreshold() {
-  scoreThreshold = Math.max(faceapi.round(scoreThreshold - 0.1), 0.1);
-  $("#scoreThreshold").val(scoreThreshold);
-  updateResults();
-}
-
-function onIncreaseMinFaceSize() {
-  minFaceSize = Math.min(faceapi.round(minFaceSize + 20), 300);
-  $("#minFaceSize").val(minFaceSize);
-}
-
-function onDecreaseMinFaceSize() {
-  minFaceSize = Math.max(faceapi.round(minFaceSize - 20), 50);
-  $("#minFaceSize").val(minFaceSize);
-}
-
 function getCurrentFaceDetectionNet() {
   if (selectedFaceDetector === SSD_MOBILENETV1) {
     return faceapi.nets.ssdMobilenetv1;
@@ -86,41 +39,9 @@ function isFaceDetectionModelLoaded() {
 }
 
 async function changeFaceDetector(detector) {
-  [
-    "#ssd_mobilenetv1_controls",
-    "#tiny_face_detector_controls",
-    "#mtcnn_controls"
-  ].forEach(id => $(id).hide());
-
   selectedFaceDetector = detector;
-  const faceDetectorSelect = $("#selectFaceDetector");
-  faceDetectorSelect.val(detector);
-  faceDetectorSelect.material_select();
 
-  $("#loader").show();
   if (!isFaceDetectionModelLoaded()) {
     await getCurrentFaceDetectionNet().load("/");
   }
-
-  $(`#${detector}_controls`).show();
-  $("#loader").hide();
-}
-
-async function onSelectedFaceDetectorChanged(e) {
-  selectedFaceDetector = e.target.value;
-
-  await changeFaceDetector(e.target.value);
-  updateResults();
-}
-
-function initFaceDetectionControls() {
-  const faceDetectorSelect = $("#selectFaceDetector");
-  faceDetectorSelect.val(selectedFaceDetector);
-  faceDetectorSelect.on("change", onSelectedFaceDetectorChanged);
-  faceDetectorSelect.material_select();
-
-  const inputSizeSelect = $("#inputSize");
-  inputSizeSelect.val(inputSize);
-  inputSizeSelect.on("change", onInputSizeChanged);
-  inputSizeSelect.material_select();
 }
